e5bbe3e4f4f68db2e15d51239ebee9f2feecbb8d
FerDoranNie <fertimjim@gmail.com>
2017-09-22 11:48:30 -0500

    Iniciando el repositorio :cloud:

---
+++ validacionGeodata.R
@@ -0,0 +1,146 @@
+####################################
+#Creado por Fernando Dorantes Nieto
+#                                   <(°) 
+#                                     ( >)"
+#                                      /|
+####################################
+
+
+library(magrittr)
+c("data.table", "ggmap", "dplyr", "tidyr", 
+  "lubridate", "geonames", "readxl") %>% 
+  sapply(require, character.only=T)
+
+
+setwd("~/ReposDesarollo/csvGeoData/")
+
+geocodeQueryCheck()
+
+# Agregar el nombre de usuario de geonames
+options(geonamesUsername="ferbase10")
+
+
+
+# Funciones ---------------------------------------------------------------
+Localidad <- function(longitud, latitud){
+  x <- revgeocode(c(longitud, latitud), output = "more")
+  direccion <- as.character(x$address)
+  calle <- as.character(x$route)
+  numero <- as.character(x$street_number)
+  cp <- as.character(x$postal_code)
+  colonia <- as.character(x$political)
+  ciudad <- as.character(x$locality)
+  estado <- as.character(x$administrative_area_level_1)
+  pais <- as.character(x$country)
+  
+  direccion <- ifelse(identical(direccion, character(0)), NA, direccion)
+  calle <- ifelse(identical(calle, character(0)), NA, calle)
+  numero <- ifelse(identical(numero, character(0)), NA, numero)
+  cp <- ifelse(identical(cp, character(0)), NA, cp)
+  colonia <- ifelse(identical(colonia, character(0)), NA, colonia)
+  ciudad <- ifelse(identical(ciudad, character(0)), NA, ciudad)
+  estado <- ifelse(identical(estado, character(0)), NA, estado)
+  pais <- ifelse(identical(pais, character(0)), NA, pais)
+  X <- data.frame(direccion,  calle, numero, colonia, cp, 
+                  ciudad_municipio= ciudad, estado, pais, 
+                  lat = latitud, long= longitud )
+  nombres <- names(X)
+  nombres <- sapply(nombres, paste, "_", "Verificado", sep="")
+  names(X) <- nombres
+  return(X)
+}
+
+
+
+# Datos -------------------------------------------------------------------
+
+acopio <- read.csv("acopio/acopio/Centros-de-Acopio.csv",
+         header = T, stringsAsFactors = F) 
+
+acopio <- acopio %>% 
+  data.table %>% 
+  .[, coordenadas := paste(X, Y, sep=", ")] 
+
+
+acopioLocalidad <- mapply("Localidad", longitud= acopio$X, latitud = acopio$Y,
+                         SIMPLIFY = F)                       
+
+acopioLocalidad2 <- do.call("rbind", acopioLocalidad) 
+
+acopioLocalidad2 <- acopioLocalidad2 %>% 
+  data.table %>% 
+  .[, coordenadas := paste(long_Verificado, lat_Verificado, sep=", ")] 
+
+
+acopioGeneral <- merge(data.frame(acopio), 
+                       data.frame(acopioLocalidad2), by="coordenadas")
+
+
+acopioGeneral %>% 
+  write.csv("acopio/acopio/acopioVerificado.csv", row.names=F)
+
+refugios <- read.csv("refugios/refugios/Central-Mexico-Earthquake-Shelters.csv",
+                   header = T, stringsAsFactors = F) 
+
+
+refugios <- refugios %>% 
+  data.table %>% 
+  .[, coordenadas := paste(X, Y, sep=", ")] 
+
+
+refugiosLocalidad <- mapply("Localidad", longitud= refugios$X, 
+                            latitud = refugios$Y,
+                          SIMPLIFY = F)                       
+
+
+refugiosLocalidad2 <- do.call("rbind", refugiosLocalidad) 
+
+refugiosLocalidad2 <- refugiosLocalidad2 %>% 
+  data.table %>% 
+  .[, coordenadas := paste(long_Verificado, lat_Verificado, sep=", ")] 
+
+
+refugiosGeneral <- merge(data.frame(refugios), 
+                       data.frame(refugiosLocalidad2), by="coordenadas")
+
+refugiosGeneral %>%  
+  write.csv("refugios/refugios/refugiosVerificado.csv", row.names=F)
+
+
+
+archivos <- list.files("edificioColapsado/colaborativo/Mapeo-Colaborativo-19s-DATOS-NECESITAN-VERIFICAR-/",
+           all.files = T, full.names = T, pattern = "*.csv")
+
+
+lapply(archivos, function(file){
+  x <- read.csv(file, stringsAsFactors = F)
+  # archivo <- (gsub(".*//","", file)
+  archivo <- gsub(".csv","", file, fixed=T)
+  archivo <- paste0(archivo,  "_Verificado", ".csv")
+  x <- x %>%
+    filter(X!="" | !is.na(X)) %>%
+    data.table %>%
+    .[, coordenadas := paste(X, Y, sep=", ")]
+
+  xLocalidad <- mapply("Localidad", longitud= x$X,
+                              latitud = x$Y,
+                              SIMPLIFY = F)
+  xLocalidad2 <- do.call("rbind", xLocalidad)
+
+  xLocalidad2 <- xLocalidad2 %>%
+    data.table %>%
+    .[, coordenadas := paste(long_Verificado, lat_Verificado, sep=", ")]
+
+
+  xGeneral <- merge(data.frame(x),
+                           data.frame(xLocalidad2), by="coordenadas")
+  xGeneral %>%
+    write.csv(archivo, row.names=F)
+  print(file)
+})
+
+
+
+
+
+
---
+++ verificarCiudad.R
@@ -0,0 +1,83 @@
+####################################
+#Creado por Fernando Dorantes Nieto
+#                                   <(°) 
+#                                     ( >)"
+#                                      /|
+####################################
+
+library(magrittr)
+c("data.table", "ggmap", "dplyr", "tidyr", 
+  "lubridate", "geonames", "readxl") %>% 
+  sapply(require, character.only=T)
+
+
+setwd("~/ReposDesarollo/csvGeoData/")
+
+geocodeQueryCheck()
+
+# Agregar el nombre de usuario de geonames
+options(geonamesUsername="ferbase10")
+#options(geonamesUsername="tu User Name") #####TIENES QUE PONER TU NOMBRE DE USUARIO DE GEONAMES
+
+
+#####BASE DE DATOS DEL SERVICIO POSTAL MEXICANO PARA LA CIUDAD DE MÉXICO
+codigos <- read.csv("CodigosPostalesCiudadMexico.csv", header = T, 
+                    stringsAsFactors = F)
+
+codigos2 <- codigos %>% 
+  select(Código.Postal, Municipio) %>% 
+  data.table %>% 
+  .[, Código.Postal := as.numeric(Código.Postal)]
+
+names(codigos2)<- c("cp", "ciudad_municipio" )
+# Funciones ---------------------------------------------------------------
+###Usarse en casos excepcionales
+
+codigoPostal <- function(postal){
+  postal <- as.numeric(postal)
+  busqueda <- GNpostalCodeLookup(postalcode=postal)
+  busqueda <- busqueda %>% 
+    filter(countryCode=="MX")
+  print(busqueda)
+  #print(busqueda$placeName)
+  postal <- as.character(postal)
+  postal <-ifelse(nchar(postal)<4, paste0("0", postal), postal)
+  Y <-data.frame(Verificado = busqueda$adminName2,
+                 cp = postal)
+  return(unique(Y))
+}
+
+archivos <- list.files("verificadosTotal",
+                       all.files = T, full.names = T, pattern = "*.csv",
+                       recursive = T)
+
+
+
+lapply(archivos, function(X){
+  file <- X
+  file <- gsub(".*/","", file)
+  file <- paste0("verificadosVuelta2Total/", file)
+  X <- read.csv(X, header = T, 
+                stringsAsFactors = F)
+  
+  X1 <- X %>%  
+    filter(ciudad_municipio_Verificado!= "Ciudad de México")
+  
+  X2 <- X %>%  
+    filter(ciudad_municipio_Verificado == "Ciudad de México")
+  
+  X2 <- merge(X2, codigos2, by.x="cp_Verificado", by.y="cp") %>% 
+    data.table %>% 
+    .[, ciudad_municipio_Verificado := ciudad_municipio ] %>% 
+    select(one_of(names(X2)))
+  
+  X <- rbind(X1, X2)
+  X <- data.frame(X)
+  X %>% 
+   write.csv(file, row.names=F)
+})
+
+
+
+
+
